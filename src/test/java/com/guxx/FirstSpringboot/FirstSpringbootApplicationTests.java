f68c6321b51d1525742de538bf74c6ef49a688e5
顾显雄 <guxianxiong@insaic.com>
2020-04-26 15:35:58 +0800

    first commit

---
+++ src/test/java/com/guxx/FirstSpringboot/FirstSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guxx.FirstSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
